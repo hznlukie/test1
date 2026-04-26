8522d4cba98d69a41e3cdf8db1939be104ff50fb
lukas <lukynsalac@gmail.com>
2026-04-26 17:40:19 +0200

    village v1

--- main.cpp/default (1) - kopie.cpp
+++ main.cpp/default (1) - kopie.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(){
-cout << "kolik vam je let: ";
+//variables
+int hp = 100;
+int mana = 100;
+int attack = 10;
+int maxhp = 100;
+
+//Village
+cout << "vitej ve vesnici" << endl;
+cout << "co potrebujes? " << endl;
+cout << "spat (hp heal, press 1)" << endl;
+cout << "behat (max hp increase, press 2)" << endl;
+cout << "trenink (damage increase, press 3)" << endl;
+cout << "zen-trenink (mana increase, press 4)" << endl;
+
+cout <<"Staty: " << endl;
+cout << "hp: " << hp << endl;
+cout << "max hp: " << maxhp << endl;
+cout << "attack: " << attack << endl;
+cout << "max mana: " << mana << endl;
 int a;
 cin >> a;
-cout << "jsi clenem klubu: ";
-string b;
-cin >> b;
-cout << "cena nakupu: ";
-double c;
-cin  >> c;
-double d;
-double e;
-if (a < 3) {
-    cout << "Jeste jste moc mlady, pouzijte ucet rodice.";
-} else if ((a >= 3) && (a <= 17) && (b == "ano")) {
-    d = c * ((100 - 30 ) * 0.01);
-    e = 30;
-} else if ((a >= 3) && (a <= 17) && (b == "ne")) {
-    d = c * ((100 - 10) * 0.01);
-    e = 10;
-}  else if ((a >= 18) && (a <= 60) && (b == "ano")) {
-  d = c * ((100 - 20) * 0.01);
-  e = 20;
 
-} else if ((a >= 18) && (a <= 60) && (b == "ne")) {
- d = c * ((100 - 0) * 0.01);
-    e = 0;
-} else if (a > 60) {
-d = c * ((100 - 40) * 0.01);
-    e = 40;
-} cout << "zadany vek: " << a << endl;
-cout << "clen klubu: " << b << endl;
-cout << "sleva: " << e << "%" << endl;
-cout << "konecna cena: " << d ;
+//village odpovedy
+if (a == 1){
+    cout << "spal si tři hodiny. 20% hp restored" << endl;
+    hp = maxhp/5+hp;
+    if (hp > maxhp){
+        hp = maxhp;
+    }
+}
+if (a == 2){
+    cout << "ubehl si par metru ¯\_(ツ)_/¯ max hp increased by 5% ";
+    maxhp = maxhp+maxhp/20;
+}
+if (a == 3){
+    cout << "trenoval si... clap clap. damage increased by 10% ";
+    attack = attack+attack/10;
+}
+if (a == 4){
+    cout << "mmmm.. dorimeeee mana increased by 5% ";
+    mana=mana+mana/20;
+}
+
+
+
+
+
+
 }
