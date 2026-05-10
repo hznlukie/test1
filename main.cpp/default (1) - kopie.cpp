2e99ccb2f467a6b39a519b0ddaefcd0aa48a2ec5
lukas <lukynsalac@gmail.com>
2026-05-10 17:15:00 +0200

    another week idk atp

--- main.cpp/default (1) - kopie.cpp
+++ main.cpp/default (1) - kopie.cpp
@@ -2,57 +2,94 @@
 #include <thread>
 #include <chrono>
 using namespace std;
+//random stttring globally, cuz c++ is literally trolling me. i swear it has agenda, i put string down locally, and c++ was like "erm actually i dont know what special attack is, it wasnt declared🤓" when i tried calling the function....
+string specialattack1;
+string specialattack2;
 
+
+
+//jdu delat prvni funkci v mem zivote, to dopadne skvele
+// PS... vysral sem se na to, moc tezky, tak priste.
+//i was risen by gods, forged from blood and tears, to conquer.... stupid functions
+void fight(int& maxhp, int& hp,
+int& maxmana, int& mana,
+int& level, int& xp,
+int& gold, int& attack, int monsterattack)
+{
+    cout << "wild pacholik attacks you!     ";
+    cout << "co je tvuj prvni tah!?" << endl;
+    cout << "vyber si z" << endl;
+    cout << "attack           " << specialattack1 << "            " << specialattack2;
+    //kurva to je v pici, ja nemam zadnou logiku na special utoky, vsak kazdy character ma jine...
+//tak treba ctvrku sem vyresil, zbytek se spravi potom.
+
+}
 int main(){
+    //variables - problem of my future self---- present self, yep thats me, its pain.
+int maxhp;
+int hp;
+int maxmana;
+int mana;
+int level;
+int xp;
+int gold;
+int attack;
+
+
+
             //zasrany postavy jebany
 //jebanin paladin
-this_thread::sleep_for(chrono::milliseconds(2000));
+bool confirmed = false;
+
+while (confirmed == false) {
+this_thread::sleep_for(chrono::milliseconds(1000));
+cout << endl;
 cout << "                                   zde jsou tve postavy za ktere muzes hrat: " << endl << endl;
-this_thread::sleep_for(chrono::milliseconds(2000));
+this_thread::sleep_for(chrono::milliseconds(1000));
 cout << " -------bratr facepalm------- "; this_thread::sleep_for(chrono::milliseconds(1000));
 cout << " -------lesni smejd------- "; this_thread::sleep_for(chrono::milliseconds(1000));
 cout <<  " -------chodici katastrofa------- ";this_thread::sleep_for(chrono::milliseconds(1000));
 cout << " -------sebevrah------- " << endl; this_thread::sleep_for(chrono::milliseconds(1000));
 
-cout << " maxHP: 100 "; this_thread::sleep_for(chrono::milliseconds(100)); // prvni radek bratr, druhy radek smejd, treti chodici katastrofa etc
+cout << " maxHP: 200 "; this_thread::sleep_for(chrono::milliseconds(100)); // prvni radek bratr, druhy radek smejd, treti chodici katastrofa etc
 cout << "                   maxHP: 100 "; this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                maxHP: 100 "; this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                       maxHP: 100 " << endl; this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                maxHP: 50 "; this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                       maxHP: 200 " << endl; this_thread::sleep_for(chrono::milliseconds(100));
 
-cout << " current hp: 100 "; this_thread::sleep_for(chrono::milliseconds(100));
+cout << " current hp: 200 "; this_thread::sleep_for(chrono::milliseconds(100));
 cout << "              current hp: 100 "; this_thread::sleep_for(chrono::milliseconds(100));
-cout << "           current hp: 100 "; this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                  current hp: 100 " << endl; this_thread::sleep_for(chrono::milliseconds(100));
+cout << "           current hp: 50 "; this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                  current hp: 200 " << endl; this_thread::sleep_for(chrono::milliseconds(100));
 
-cout << " maxmana: 50 "; this_thread::sleep_for(chrono::milliseconds(100));
+cout << " maxmana: 20 "; this_thread::sleep_for(chrono::milliseconds(100));
 cout << "                  maxmana: 50 "; this_thread::sleep_for(chrono::milliseconds(100));
-cout << "               maxmana: 50 ";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                      maxmana: 50" << endl;this_thread::sleep_for(chrono::milliseconds(100));
+cout << "               maxmana: 100 ";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                    maxmana: 20" << endl;this_thread::sleep_for(chrono::milliseconds(100));
 
-cout << " current mana: 50 ";this_thread::sleep_for(chrono::milliseconds(100));
+cout << " current mana: 20 ";this_thread::sleep_for(chrono::milliseconds(100));
 cout << "             current mana: 50 ";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "          current mana: 50 ";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                 current mana: 50 " << endl;this_thread::sleep_for(chrono::milliseconds(100));
+cout << "          current mana: 100 ";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "               current mana: 20 " << endl;this_thread::sleep_for(chrono::milliseconds(100));
 
-cout << " gold: 2";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                       gold: 2";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                    gold: 2";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                           gold: 2" << endl;this_thread::sleep_for(chrono::milliseconds(100));
+cout << " gold: 0";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                       gold: 0";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                    gold: 0";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                          gold: 0" << endl;this_thread::sleep_for(chrono::milliseconds(100));
 
-cout << " level: 0/50";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                   level: 0/50";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                level: 0/50";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                       level: 0/50" << endl;this_thread::sleep_for(chrono::milliseconds(100));
+cout << " level: 1";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                      level: 1";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                   level: 1"; this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                         level: 1" << endl;this_thread::sleep_for(chrono::milliseconds(100));
 
-cout << " xp: 20 za bitvu";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "               xp: 20 za bitvu";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "            xp: 20 za bitvu";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                   xp: 20 za bitvu" << endl;this_thread::sleep_for(chrono::milliseconds(100));
+cout << " xp: 0/50";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                      xp: 0/50";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                   xp: 0/50";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                         xp: 0/50" << endl;this_thread::sleep_for(chrono::milliseconds(100));
 
-cout << " attack: 20 ";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                   attack: 20 ";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                attack: 20 ";this_thread::sleep_for(chrono::milliseconds(100));
-cout << "                       attack: 20 " << endl;this_thread::sleep_for(chrono::milliseconds(100));
+cout << " attack: 30 ";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                   attack: 30 ";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                attack: 30 ";this_thread::sleep_for(chrono::milliseconds(100));
+cout << "                      attack: 40 " << endl;this_thread::sleep_for(chrono::milliseconds(100));
 
 //poty sracce co jsem to zkurvil, tak tu mrdam actually code jak choosovat and proper logic
 string odpoved1;
@@ -65,25 +102,92 @@ if (cin.peek() == ' ') {
 }
 
 if (odpoved1 == "bratr" && odpoved2 == "facepalm"){
-cout << "success";
+cout << "vybrali ste si bratr facepalm, ste si jisty? (ano/ne)";
+string potvrzeni;
+cin >> potvrzeni;
+
+if (potvrzeni == "ano") {
+    confirmed = true;
+maxhp=200;
+hp = 200;
+maxmana=20;
+mana=20;
+level=1;
+xp=0;
+gold=0;
+attack=30;
+specialattack1= "shield bash"; //stun
+specialattack2= "divine heal"; //heal
+}
 }
 else if (odpoved1 == "lesni" && odpoved2 == "smejd"){
-cout << "success";
+cout << "vybrali ste si lesni smejd, ste si jisty? (ano/ne)";
+string potvrzeni;
+cin >> potvrzeni;
+
+if (potvrzeni == "ano") {
+    confirmed = true;
+maxhp=100;
+hp = 100;
+maxmana=50;
+mana=50;
+level=1;
+xp=0;
+gold=0;
+attack=30;
+specialattack1= "poison arrow"; //dmg over time
+specialattack2= "trap"; //stun
+}
 }
 else if (odpoved1 == "chodici" && odpoved2 == "katastrofa"){
-cout << "success";
+cout << "vybrali ste si chodici katastrofa, ste si jisty? (ano/ne)";
+string potvrzeni;
+cin >> potvrzeni;
+
+if (potvrzeni == "ano") {
+    confirmed = true;
+maxhp=50;
+hp = 50;
+maxmana=100;
+mana=100;
+level=1;
+xp=0;
+gold=0;
+attack=30;
+specialattack1= "fireball"; //hits all enemies
+specialattack2= "frost nova"; //stun, jakoby freeze ale stun proste..
+}
 }
 else if (odpoved1 == "sebevrah"){
-cout << "success";
+cout << "vybrali ste si sebevrah, ste si jisty? (ano/ne)";
+string potvrzeni;
+cin >> potvrzeni;
+
+if (potvrzeni == "ano") {
+    confirmed = true;
+maxhp=200;
+hp = 200;
+maxmana=20;
+mana=20;
+level=1;
+xp=0;
+gold=0;
+attack=40;
+specialattack1= "life drain"; //causes dmg and heals for it
+specialattack2= "curse"; //reduce enemy dmg
+}
 } else {
     cout << "invalid choice";
-}
-//to nad timhle musis dodelat vole logika dobra, text k hovnu yeeey
-//variables - problem of my future self
-int hp = 100;
-int mana = 100;
-int attack = 10;
-int maxhp = 100;
+}}
+//past self - to nad timhle musis dodelat vole logika dobra, text k hovnu yeeey.... present self - finished hahah zmrde, sem lepsi...
+
+
+
+
+//proc tu je dira... tady neco ma byt???
+
+
+
 
 //Village zatim jedina vec co se nepokusila mi dat deprese
 cout << "vitej ve vesnici" << endl;
@@ -110,7 +214,7 @@ if (a == 1){
     }
 }
 if (a == 2){
-    cout << "ubehl si par metru ¯\_(ツ)_/¯ max hp increased by 5% ";
+    cout << "ubehl si par metru ¯\\_(ツ)_/¯ max hp increased by 5% ";
     maxhp = maxhp+maxhp/20;
 }
 if (a == 3){
