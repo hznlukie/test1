34c597708cf9aecb2414c18025faaaee0401c1ff
lukas <lukynsalac@gmail.com>
2026-05-31 13:53:43 +0200

    before final version

--- main.cpp/default (1) - kopie.cpp
+++ main.cpp/default (1) - kopie.cpp
@@ -2,8 +2,8 @@
 #include <thread>
 #include <chrono>
 #include <cstdlib> // crash game
+#include <limits>
 using namespace std;
-//random stttring globally, cuz c++ is literally trolling me. i swear it has agenda, i put string down locally, and c++ was like "erm actually i dont know what special attack is, it wasnt declared🤓" when i tried calling the function....
 string specialattack1;
 string specialattack2;
 int monsterstun = 0;
@@ -17,15 +17,81 @@ int poisondamage2 =0;
 int check =0;
 int jakynepritel = 67;
 
+void village(int &hp, int &maxhp, int &attack, int &mana, int maxmana)
+{
+    cout << "vitej ve vesnici" << endl;
+    cout << "co potrebujes? " << endl;
+    cout << "spat (hp heal, press 1)" << endl;
+    cout << "behat (max hp increase, press 2)" << endl;
+    cout << "trenink (damage increase, press 3)" << endl;
+    cout << "zen-trenink (mana fill, press 4)" << endl;
 
-//jdu delat prvni funkci v mem zivote, to dopadne skvele
-// PS... vysral sem se na to, moc tezky, tak priste.
-//Verily, I was raised by the gods themselves, and forged in blood and tears, that I might, with solemn purpose, conquer… these most wretched and ill-conceived of functions, prithee.
+    cout << "Staty: " << endl;
+    cout << "hp: " << hp << endl;
+    cout << "max hp: " << maxhp << endl;
+    cout << "attack: " << attack << endl;
+    cout << "max mana: " << mana << endl;
+
+    int a;
+    cin >> a;
+    if (a == 1){
+        cout << "spal si tři hodiny. 20% hp restored" << endl;
+        hp = maxhp/5 + hp;
+        if (hp > maxhp){
+            hp = maxhp;
+        }
+    }
+
+    if (a == 2){
+        cout << "ubehl si par metru ¯\\_(ツ)_/¯ max hp increased by 5% ";
+        maxhp = maxhp + maxhp/20;
+    }
+
+    if (a == 3){
+        cout << "trenoval si... clap clap. damage increased by 10% ";
+        attack = attack + attack/10;
+    }
+
+    if (a == 4){
+        cout << "mmmm.. dorimeeee mana increased by 50% ";
+        mana = mana + mana/2;
+        if (mana > maxmana){
+            mana = maxmana;
+        }
+    }
+}
+
+
+
+
+void checkLevelUp(int& level, int& xp, int& maxhp, int& attack) {
+    if (xp >= 50) {
+        level++;
+        xp = 0;
+        maxhp += 5;
+        attack += 2;
+
+        cout << "LEVEL UP!" << endl;
+        cout << "Max HP +5, Attack +2" << endl;
+    }
+}
+
+
+
+
+
+
+
+
+
 void fight(int& maxhp, int& hp,
 int& maxmana, int& mana,
 int& level, int& xp,
 int& gold, int& attack, int monsterattack, int monsterhp)
 {
+    monsterstun = 0;
+    poisondamage= 0;
+
     cout << "wild pacholik attacks you!     " << endl; this_thread::sleep_for(chrono::milliseconds(1000));
     while (hp > 0 && monsterhp > 0){
     cout << "co je tvuj tah!?" << endl; this_thread::sleep_for(chrono::milliseconds(1000));
@@ -33,52 +99,80 @@ int& gold, int& attack, int monsterattack, int monsterhp)
     while (check == 0){
     cout << "vyber si cislo" << endl; this_thread::sleep_for(chrono::milliseconds(1000));
     cout << "1 - attack                    "; this_thread::sleep_for(chrono::milliseconds(1000)); cout << endl; cout << "2 - " << specialattack1; this_thread::sleep_for(chrono::milliseconds(1000)); cout << endl; cout << "3 - " << specialattack2;
-cin >> vyber;
+cout << endl;
+cout << "potrebujete dostatek many na special utoky, 10.. a mate " << mana << " many";
+cout << endl;
+if (!(cin >> vyber)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "invalid input";
+    continue;
+}
+if ((vyber == 2 || vyber == 3) && mana < 10) {
+    cout << "not enough mana!" << endl;
+    continue;
+}
     if (vyber==1){
+        cout << "ubral jste mu " << attack << " damage.. ted ma " ;
         monsterhp= monsterhp - attack;
         check = 1;
+        cout << monsterhp << " zivotu" << endl;
     }
-    //the most disguisting inhumane code i ever formed
-    else if (vyber == 2 && helpmevariable == 1) {
-cout << "omracil ste to!";
+// if else chain
+    else if (vyber == 2 && helpmevariable == 1 && mana >= 10) {
+cout << "omracil ste to!" << endl;
 monsterstun = 1;
  check = 1;
+ mana -= 10;
 }
-        else if (vyber == 2 && helpmevariable == 2) {
+        else if (vyber == 2 && helpmevariable == 2 && mana >= 10) {
+cout << "dal jste mu poison" << endl;
 poisondamage=2;
  check = 1;
+ mana -= 10;
 }
-            else if (vyber == 2 && helpmevariable == 3) {
-//multiple enemies damage, again future myself problem
+            else if (vyber == 2 && helpmevariable == 3 && mana >= 10) {
+cout << "zautocil jste fireball za " << attack << " damage" << endl;
+monsterhp = monsterhp - attack;
  check = 1;
+ mana -= 10;
+        cout <<  "ted ma" << monsterhp <<" zivotu" << endl;
 }
-                else if (vyber == 2 && helpmevariable == 4) {
-monsterhp= monsterhp - (attack+2);
+                else if (vyber == 2 && helpmevariable == 4 && mana >= 10) {
+cout << "zautocil jste life stealem za " << attack+10 << " damage a healnul jsi se o " << attack/2 << " hp" << endl;
+monsterhp= monsterhp - (attack+10);
 hp=hp+attack/2;
 if (hp > maxhp) hp = maxhp;
  check = 1;
+ mana -= 10;
+         cout <<  "ted ma " << monsterhp <<" zivotu" << endl;
 }
-                    else if (vyber == 3 && helpmevariable == 1) {
-cout << "healnul jsi se";
+                    else if (vyber == 3 && helpmevariable == 1 && mana >= 10) {
+cout << "healnul jsi se o 20 zivotu" ;
 hp=hp+20;
 if (hp>maxhp){
     hp=maxhp;
 }
  check = 1;
+ mana -= 10;
 }
-                        else if (vyber == 3 && helpmevariable == 2) {
+                        else if (vyber == 3 && helpmevariable == 2 && mana >= 10) {
 cout << "trapnul ste to!";
 monsterstun = 1;
  check = 1;
+ mana -= 10;
 }
-                            else if (vyber == 3 && helpmevariable == 3) {
+                            else if (vyber == 3 && helpmevariable == 3 && mana >= 10) {
 cout << "zmrazil ste to!";
 monsterstun = 1;
  check = 1;
+ mana -= 10;
 }
-                                else if (vyber == 3 && helpmevariable == 4) {
+                                else if (vyber == 3 && helpmevariable == 4 && mana >= 10) {
+ cout << "zeslabil jste to "<< endl;
  monsterattack=monsterattack-monsterattack/5;
  check = 1;
+ mana -= 10;
 }
                                         else {
 cout << "zadejte valid option ";
@@ -102,10 +196,7 @@ cout << "monster je mrtev";
     this_thread::sleep_for(chrono::milliseconds(2000));
     exit(0);
     }
-    //kurva to je v pici, ja nemam zadnou logiku na special utoky, vsak kazdy character ma jine...
-//tak treba ctvrku sem vyresil, zbytek se spravi potom.
-// sigh,.. ted je to potom
-//tak je to jeste vetsi problem... kazdy ma jinou schopnost s jinymi efekty... ja HAHAHAHHHAAAHAHA budu muset delat dalsi ctyri variables ahhahahahahha
+
 }
 
 
@@ -115,6 +206,11 @@ int& maxmana, int& mana,
 int& level, int& xp,
 int& gold, int& attack, int monsterattack1, int monsterhp1, int monsterattack2, int monsterhp2)
 {
+    monsterstun1 = 0;
+    monsterstun2 = 0;
+    poisondamage1 = 0;
+    poisondamage2 = 0;
+
     cout << "wild pacholiks attack you!     " << endl; this_thread::sleep_for(chrono::milliseconds(1000));
     while (hp > 0 && (monsterhp1 > 0 || monsterhp2 > 0)){
     cout << "co je tvuj tah!?" << endl; this_thread::sleep_for(chrono::milliseconds(1000));
@@ -122,25 +218,40 @@ int& gold, int& attack, int monsterattack1, int monsterhp1, int monsterattack2, 
     while (check == 0){
 
     cout << "vyber si cislo" << endl; this_thread::sleep_for(chrono::milliseconds(1000));
-    cout << "1 - attack                    "; this_thread::sleep_for(chrono::milliseconds(1000)); cout << endl; cout << "2 - " << specialattack1; this_thread::sleep_for(chrono::milliseconds(1000)); cout << endl; cout << "3 - " << specialattack2;
-cin >> vyber;
+    cout << "1 - attack                    "; this_thread::sleep_for(chrono::milliseconds(1000)); cout << endl; cout << "2 - " << specialattack1; this_thread::sleep_for(chrono::milliseconds(1000)); cout << endl; cout << "3 - " << specialattack2 << endl;
+if (!(cin >> vyber)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "invalid input";
+    continue;
+}
+if ((vyber == 2 || vyber == 3) && mana < 10) {
+    cout << "not enough mana!" << endl;
+    continue;
+}
+if (!(helpmevariable == 1 && vyber == 3)) {
+jakynepritel = 0;
 while (jakynepritel != 1 && jakynepritel != 2 || (jakynepritel == 1 && monsterhp1 <= 0) || (jakynepritel == 2 && monsterhp2 <= 0)) //tough condition as hell, i am proud
     {
 
-cout << "vyber si nepritele, 1, 2 (ps, musi zit     ";
+cout << "vyber si nepritele " << endl;
+cout << "PS, musi zit" << endl;
 if (monsterhp1 >0){
-    cout << " monster 1 zije";
-} else {cout << " monster 1 nezije";}
+    cout << "monster 1:                 zije";
+} else {cout << "monster 1:           nezije";}
 if (monsterhp2 >0){
-    cout << " monster 2 zije";
-} else {cout << " monster 2 nezije";}
+    cout << endl << "monster 2:                 zije" << endl;
+} else {cout << endl << "monster 2:             nezije" << endl;}
 
-cin >> jakynepritel;
+
+
+if (!(cin >> jakynepritel)) {
     cin.clear();
-    cin.ignore(1000, '\n');
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
-    //the most disguisting inhumane code i ever formed. i lied before, this is even WORSE.. even satan would fear me...
-    if (vyber==1 && jakynepritel ==1){
+}}
+    //else if chain 2
+    if (vyber==1 && jakynepritel ==1 ){
     monsterhp1= monsterhp1 - attack;
     check = 1;
     }
@@ -149,76 +260,93 @@ cin >> jakynepritel;
         check = 1;
     }
 
-            else if (vyber == 2 && helpmevariable == 1 && jakynepritel == 1) {
+            else if (vyber == 2 && helpmevariable == 1 && jakynepritel == 1 && mana >= 10) {
             cout << "omracil ste prvniho pacholika!";
             monsterstun1 = 1;
             check = 1;
-}               else if (vyber == 2 && helpmevariable == 1 && jakynepritel == 2) {
+            mana -= 10;
+}               else if (vyber == 2 && helpmevariable == 1 && jakynepritel == 2 && mana >= 10) {
                 cout << "omracil ste druhyho pacholika!";
                 monsterstun2 = 1;
                 check = 1;
+                mana -= 10;
 }
-                    else if (vyber == 2 && helpmevariable == 2 && jakynepritel == 1) {
+                    else if (vyber == 2 && helpmevariable == 2 && jakynepritel == 1 && mana >= 10) {
                     poisondamage1=2;
                     check = 1;
-}                       else if (vyber == 2 && helpmevariable == 2 && jakynepritel == 2) {
+                    mana -= 10;
+}                       else if (vyber == 2 && helpmevariable == 2 && jakynepritel == 2 && mana >= 10) {
                         poisondamage2=2;
                         check = 1;
+                        mana -= 10;
 }
-                            else if (vyber == 2 && helpmevariable == 3 && jakynepritel == 1) {
+                            else if (vyber == 2 && helpmevariable == 3 && jakynepritel == 1 && mana >= 10) {
                             monsterhp1 = monsterhp1 - attack;
                             monsterhp2 = monsterhp2 - attack/2;
                             check = 1;
-}                               else if (vyber == 2 && helpmevariable == 3 && jakynepritel == 2) {
+                            mana -= 10;
+}                               else if (vyber == 2 && helpmevariable == 3 && jakynepritel == 2 && mana >= 10) {
                                 monsterhp1 = monsterhp1 - attack/2;
                                 monsterhp2 = monsterhp2 - attack;
                                 check = 1;
+                                mana -= 10;
 }
-                                    else if (vyber == 2 && helpmevariable == 4 && jakynepritel == 1) {
+                                    else if (vyber == 2 && helpmevariable == 4 && jakynepritel == 1 && mana >= 10) {
                                     monsterhp1= monsterhp1 - (attack+2);
                                     hp=hp+attack/2;
                                     if (hp > maxhp) {hp = maxhp;}
                                     check = 1;
-}                                       else if (vyber == 2 && helpmevariable == 4 && jakynepritel == 2) {
+                                    mana -= 10;
+
+}                                       else if (vyber == 2 && helpmevariable == 4 && jakynepritel == 2 && mana >= 10) {
                                         monsterhp2= monsterhp2 - (attack+2);
                                         hp=hp+attack/2;
                                         if (hp > maxhp) {hp = maxhp;}
                                         check = 1;
+                                        mana -= 10;
 }
-                                            else if (vyber == 3 && helpmevariable == 1) {
+                                            else if (vyber == 3 && helpmevariable == 1 && mana >= 10) {
                                             cout << "healnul jsi se";
                                             hp=hp+20;
                                             if (hp>maxhp){
                                             hp=maxhp;
                                             }
                                             check = 1;
+                                            mana -= 10;
 }
-                                                else if (vyber == 3 && helpmevariable == 2 && jakynepritel == 1) {
+                                                else if (vyber == 3 && helpmevariable == 2 && jakynepritel == 1 && mana >= 10) {
                                                 cout << "trapnul si tu prvni monstrum!";
                                                 monsterstun1 = 1;
                                                 check = 1;
-}                                                   else if (vyber == 3 && helpmevariable == 2 && jakynepritel == 2) {
+                                                mana -= 10;
+
+}                                                   else if (vyber == 3 && helpmevariable == 2 && jakynepritel == 2 && mana >= 10) {
                                                     cout << "trapnul si tu druhe monstrum!";
                                                     monsterstun2 = 1;
                                                     check = 1;
+                                                    mana -= 10;
 }
-                                                        else if (vyber == 3 && helpmevariable == 3 && jakynepritel == 1) {
+                                                        else if (vyber == 3 && helpmevariable == 3 && jakynepritel == 1 && mana >= 10) {
                                                         cout << "zmrazil ste to prvni!";
                                                         monsterstun1 = 1;
                                                         check = 1;
+                                                        mana -= 10;
 }
-                                                            else if (vyber == 3 && helpmevariable == 3 && jakynepritel == 2) {
+                                                            else if (vyber == 3 && helpmevariable == 3 && jakynepritel == 2 && mana >= 10) {
                                                             cout << "zmrazil ste to druhe!";
                                                             monsterstun2 = 1;
                                                             check = 1;
+                                                            mana -= 10;
 }
-                                                                else if (vyber == 3 && helpmevariable == 4 && jakynepritel == 1) {
+                                                                else if (vyber == 3 && helpmevariable == 4 && jakynepritel == 1 && mana >= 10) {
                                                                 monsterattack1=monsterattack1-monsterattack1/5;
                                                                 check = 1;
+                                                                mana -= 10;
 }
-                                                                    else if (vyber == 3 && helpmevariable == 4 && jakynepritel == 2) {
+                                                                    else if (vyber == 3 && helpmevariable == 4 && jakynepritel == 2 && mana >= 10) {
                                                                     monsterattack2=monsterattack2-monsterattack2/5;
                                                                     check = 1;
+                                                                    mana -= 10;
 }
                                                                         else {
                                                                         cout << "zadejte valid option ";
@@ -262,13 +390,6 @@ cin >> jakynepritel;
     }
 
 }
-   //stejna funkce. jen pro dva monstry.. Nurunuru shite kita... Tasukete hoshii n da kedo...
-
-
-
-
-
-
 
 
 
@@ -276,7 +397,7 @@ cin >> jakynepritel;
 
 
 int main(){
-    //variables - problem of my future self---- present self, yep thats me, its pain.
+
 int maxhp;
 int hp;
 int maxmana;
@@ -287,8 +408,7 @@ int gold;
 int attack;
 
 
-            //zasrany postavy jebany
-//jebanin paladin
+
 bool confirmed = false;
 
 while (confirmed == false) {
@@ -305,7 +425,7 @@ cout << "\033[2A";
 cout << "\033[54C";
 
 cout << "FEAR THE EMPEROR" << flush;
-//ich heisse kaiser
+
 cout << "\033[2B";
 cout << endl;
 
@@ -358,7 +478,7 @@ cout << "                   attack: 30 ";this_thread::sleep_for(chrono::millisec
 cout << "                attack: 30 ";this_thread::sleep_for(chrono::milliseconds(50));
 cout << "                      attack: 40 " << endl;this_thread::sleep_for(chrono::milliseconds(50));
 cout << "co si vyberes? " << endl;
-//poty sracce co jsem to zkurvil, tak tu mrdam actually code jak choosovat and proper logic
+
 string odpoved1;
 string odpoved2;
 cin >> odpoved1;
@@ -449,56 +569,9 @@ helpmevariable = 4; // function logic on line 29
 } else {
     cout << "invalid choice";
 }}
-//past self - to nad timhle musis dodelat vole logika dobra, text k hovnu yeeey.... present self - finished hahah zmrde, sem lepsi...
 
 
+fight(maxhp, hp, maxmana, mana, level, xp, gold,attack, 10, 90);
 
 
-//proc tu je dira... tady neco ma byt???
-
-// to be or not to be.. philosophical KVASTIOOOOOOON
-
-fightmultipleenemies(maxhp, hp, maxmana, mana, level, xp, gold, attack, 10, 50, 10, 50);
-//Village zatim jedina vec co se nepokusila mi dat deprese
-cout << "vitej ve vesnici" << endl;
-cout << "co potrebujes? " << endl;
-cout << "spat (hp heal, press 1)" << endl;
-cout << "behat (max hp increase, press 2)" << endl;
-cout << "trenink (damage increase, press 3)" << endl;
-cout << "zen-trenink (mana increase, press 4)" << endl;
-
-cout <<"Staty: " << endl;
-cout << "hp: " << hp << endl;
-cout << "max hp: " << maxhp << endl;
-cout << "attack: " << attack << endl;
-cout << "max mana: " << mana << endl;
-int a;
-cin >> a;
-
-//village odpovedy, ani nevim co tohle je kurva, sem to predtim nepopsal
-if (a == 1){
-    cout << "spal si tři hodiny. 20% hp restored" << endl;
-    hp = maxhp/5+hp;
-    if (hp > maxhp){
-        hp = maxhp;
-    }
-}
-if (a == 2){
-    cout << "ubehl si par metru ¯\\_(ツ)_/¯ max hp increased by 5% ";
-    maxhp = maxhp+maxhp/20;
-}
-if (a == 3){
-    cout << "trenoval si... clap clap. damage increased by 10% ";
-    attack = attack+attack/10;
-}
-if (a == 4){
-    cout << "mmmm.. dorimeeee mana increased by 5% ";
-    mana=mana+mana/20;
-}
-
-
-
-
-
-//01110111011010000111100100100000011000010111001001100101001000000111100101101111011101010010000001101000011001010111001001100101
 }
